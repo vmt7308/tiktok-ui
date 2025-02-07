94c74c8daacc6c67c8d55d203157048cffe8eac4
Minh Trung <vmt7308@gmail.com>
2025-02-08 00:08:56 +0700

    Building Header Logic #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,14 +1,19 @@
-import classNames from "classnames/bind";
-import styles from "./AccountItem.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from '~/components/Image';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return ( 
+    return (
         <div className={cx('wrapper')}>
-            <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/aweme/1080x1080/tos-alisg-avt-0068/79f076e483316ba016b92f05452e343c.jpeg?lk3s=a5d48078&nonce=80289&refresh_token=c58a4ff5a7ef5d3b98486fbd8b41a6a5&x-expires=1739012400&x-signature=yBfYA7UpLbP%2BgNBgaR26Z0sUjvU%3D&shp=a5d48078&shcp=81f88b70" alt="hoaa" />
+            <Image
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/aweme/1080x1080/tos-alisg-avt-0068/79f076e483316ba016b92f05452e343c.jpeg?lk3s=a5d48078&nonce=80289&refresh_token=c58a4ff5a7ef5d3b98486fbd8b41a6a5&x-expires=1739012400&x-signature=yBfYA7UpLbP%2BgNBgaR26Z0sUjvU%3D&shp=a5d48078&shcp=81f88b70"
+                alt="hoaa"
+            />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
                     <span>Nguyễn Trần Thị Hoa</span>
