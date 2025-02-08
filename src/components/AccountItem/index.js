ae5a44386525f9f8f32b4c4237432d48be5b5ba8
Minh Trung <vmt7308@gmail.com>
2025-02-08 19:46:23 +0700

    Building Header Logic #2

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,27 +1,24 @@
-import classNames from 'classnames/bind';
-import styles from './AccountItem.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Image from '~/components/Image';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import Image from '~/components/Image';
+import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://p16-sign-sg.tiktokcdn.com/aweme/1080x1080/tos-alisg-avt-0068/79f076e483316ba016b92f05452e343c.jpeg?lk3s=a5d48078&nonce=80289&refresh_token=c58a4ff5a7ef5d3b98486fbd8b41a6a5&x-expires=1739012400&x-signature=yBfYA7UpLbP%2BgNBgaR26Z0sUjvU%3D&shp=a5d48078&shcp=81f88b70"
-                alt="hoaa"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyễn Trần Thị Hoa</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>hoanguyen</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
